f31f766b6dee58a435396f9b42f43561aa3bb712
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-04-22 00:36:52 +0100

    Refactor manifest

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -40,21 +40,10 @@ package { 'git-core':
 
 # --- Ruby ---------------------------------------------------------------------
 
-exec { 'install_rvm':
-  command => "${as_vagrant} 'curl -L http://get.rvm.io | bash -s stable'",
+exec { 'install_ruby':
+  command => "${as_vagrant} 'curl -sSL https://get.rvm.io | bash -s stable --ruby=${ruby_version}'",
   creates => "${home}/.rvm/bin/rvm",
   require => Package['curl']
-}
-
-exec { 'install_ruby':
-  # We run the rvm executable directly because the shell function assumes an
-  # interactive environment, in particular to display messages or ask questions.
-  # The rvm executable is more suitable for automated installs.
-  #
-  # use a ruby patch level known to have a binary
-  command => "${as_vagrant} '${home}/.rvm/bin/rvm install ruby-${ruby_version} --binary --autolibs=enabled && rvm alias create default ${ruby_version}'",
-  creates => "${home}/.rvm/bin/ruby",
-  require => Exec['install_rvm']
 }
 
 # RVM installs a version of bundler, but for edge Rails we want the most recent one.
@@ -62,12 +51,6 @@ exec { 'install_bundle':
   command => "${as_vagrant} 'gem install bundler --no-rdoc --no-ri'",
   creates => "${home}/.rvm/bin/bundle",
   require => Exec['install_ruby']
-}
-
-# Bundle install
-exec { 'bundle_install':
-  command => "${as_vagrant} 'bundle install --gemfile /vagrant/Gemfile --quiet'",
-  require => Exec['install_bundle']
 }
 
 # --- Locale -------------------------------------------------------------------
