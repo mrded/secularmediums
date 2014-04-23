0b9822e94cf4e37d5b482dfefab31faff3179212
mrded <dmitry@demenchuk.me>
2014-04-23 21:04:14 +0100

    Add pages.

--- app.rb
+++ app.rb
@@ -6,12 +6,36 @@ class App < Sinatra::Base
   require './views/layout.rb'
 
   set :mustache, {
-    :views     => './views/',
-    :templates => './templates/'
+    views: './views/',
+    templates: './templates/'
   }
 
   get '/' do
     mustache :index
+  end
+
+  get '/your_session' do
+    mustache :your_session
+  end
+
+  get '/retrospective' do
+    mustache :retrospective
+  end
+
+  get '/remote_questioning' do
+    mustache :remote_questioning
+  end
+
+  get '/book_excerpts' do
+    mustache :book_excerpts
+  end
+
+  get '/testimonials' do
+    mustache :testimonials
+  end
+
+  get '/contact' do
+    mustache :contact
   end
 
 end
